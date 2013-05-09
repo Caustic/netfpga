07aacce321f39a630c31bb7ec177847f97631a34
Alan Braithwaite <abraithwaite@mail.csuchico.edu>
2013-05-08 19:42:15 -0700

    Added changes to openflow projct and modelsim preparation file

--- prepare_modelsim.sh
+++ prepare_modelsim.sh
@@ -3,8 +3,10 @@
 vsim_dir="${HOME}/modelsim"
 mkdir -p $vsim_dir
 files=$(find ${NF_DESIGN_DIR} -iname "*.v")
+defines=$(find "${NF_ROOT}/lib/verilog/core/common/src/" -iname "*.v")
+all="${defines} ${files}"
 
-for f in $files
+for f in $all
 do
     temp="$(basename $f)"
     if [[ ! -f "$vsim_dir/$temp" ]]
